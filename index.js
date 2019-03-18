d7acdb0d1105641c444de0455994efe0f515c979
kristjanjansen <kristjan.jansen@gmail.com>
2019-03-18 16:44:03 +0200

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { parseSheet } from './utils.js';
+import { parseSheet } from "./utils.js";
 
 Vue.component("l-map", Vue2Leaflet.LMap);
 Vue.component("l-tile-layer", Vue2Leaflet.LTileLayer);
@@ -6,7 +6,7 @@ Vue.component("l-marker", Vue2Leaflet.LMarker);
 Vue.component("l-polyline", Vue2Leaflet.LPolyline);
 Vue.component("l-geo-json", Vue2Leaflet.LGeoJson);
 
-import Event from './components/Event.js'
+import Event from "./components/Event.js";
 
 Vue.config.devtools = true;
 
@@ -19,9 +19,25 @@ new Vue({
     zoom: 9,
     center: [59, 24],
     markers: [],
-    id: '19Xj62Df8IvP-yLDpMKEWknR-ytazmbIwAIJptxHPZgA',
+    id: "19Xj62Df8IvP-yLDpMKEWknR-ytazmbIwAIJptxHPZgA",
     value: null,
-    county: null
+    county: null,
+    counties: [
+      "harjumaa",
+      "hiiumaa",
+      "jogevamaa",
+      "laanemaa",
+      "laanevirumaa",
+      "parnumaa",
+      "polvamaa",
+      "raplamaa",
+      "saaremaa",
+      "tartu",
+      "valgamaa",
+      "viljandimaa",
+      "vorumaa"
+    ],
+    c: {}
   },
   methods: {
     onClick(i) {
@@ -34,28 +50,31 @@ new Vue({
         this.id
       }/od6/public/values?alt=json`
     )
-    .then(res => res.json())
-    .then(res => {
-      this.markers = parseSheet(res).map(m => {
-        m.lat = parseFloat(m.lat)
-        m.lng = parseFloat(m.lng)
-        return m
-      })
-    });
-    fetch('./tracks/polvamaa.json')
       .then(res => res.json())
       .then(res => {
-        console.log(JSON.stringify(res))
-        this.county = res
-      })
+        this.markers = parseSheet(res).map(m => {
+          m.lat = parseFloat(m.lat);
+          m.lng = parseFloat(m.lng);
+          return m;
+        });
+      });
+
+    this.counties.forEach(c => {
+      fetch(`./tracks/${c}.json`)
+        .then(res => res.json())
+        .then(res => {
+          this.c[c] = res;
+        });
+    });
   },
   template: `
   <div style="display: flex">
     <l-map style="height: 100vh; width: 80vw" :zoom="zoom" :center="center">
       <l-tile-layer :url="url"/>
       <l-geo-json
-        v-if="county"
-        :geojson="county"
+        v-if="c"
+        v-for="(cc,i) in c"
+        :geojson="cc"
       />
       <l-marker
         v-if="markers.length"
