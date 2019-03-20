b7a961991d3c4389e66f8d935cab96940426f9b4
airiastmae <46886658+airiastmae@users.noreply.github.com>
2019-03-20 13:00:43 +0200

    Eventlist with padding error

--- components/EventList.js
+++ components/EventList.js
@@ -9,7 +9,25 @@ export default {
         :key="i"
         @click="$emit('changeEvent', event.ID)"
       >
-        <div>{{ event.name }}</div>
+        <!--<div>{{ event.name }}</div>-->
+        <div class="EventInfo">
+        <div class="EventInfo_date">
+          <h3 class="EventInfo_date_number">
+          16.
+          </h3>
+          <h5 class="EventInfo_date_month">
+          aprill
+          </h5>
+        </div>
+        <div class="EventInfo_meta">
+          <h3 class="EventInfo_name">
+          {{ event.name }}
+          </h3>
+          <p class="EventInfo_location">
+          Orjaku külamaja 19.00-02.00
+          </p>
+        </div>
+        </div>
       </div>
     </div>
   `
