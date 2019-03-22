cf850ae6c72fab57408a9abb193d2c514c0333c8
kristjanjansen <kristjan.jansen@gmail.com>
2019-03-22 10:03:51 +0200

    path fix

--- components/Top.js
+++ components/Top.js
@@ -11,8 +11,8 @@ export default {
      <!-- <h2>1.juuni - 4.juuli 2019</h2>-->
    	</div>
     <div class="Top__social">
-      <img src="/assets/logo_fb.png" />
-      <img src="/assets/logo_insta.png" />
+      <img src="assets/logo_fb.png" />
+      <img src="assets/logo_insta.png" />
     </div>
   </div>
   `
